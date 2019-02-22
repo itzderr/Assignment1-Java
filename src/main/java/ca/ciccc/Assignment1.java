ffd62b775487d6e89683426099e10f1d89f8b49b
Shu-Ming <neverland313@gmail.com>
2019-02-22 10:11:30 -0800

    Finish Assignment1 (real)

--- src/main/java/ca/ciccc/Assignment1.java
+++ src/main/java/ca/ciccc/Assignment1.java
@@ -1,5 +1,8 @@
 package ca.ciccc;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class Assignment1 {
 
     /**
@@ -24,9 +27,10 @@ public class Assignment1 {
      * @param inches
      * @return "1000.0 inches is 25.4 meters"
      */
-    public static String inchesToMeters(int inches) {
-
-        return "";
+    public static String inchesToMeters(double inches) {
+        double m = inches * 0.0254;
+        String result2 = String.format("%.1f inches is %.1f meters", inches, m);
+        return result2;
     }
 
     /**
@@ -37,8 +41,14 @@ public class Assignment1 {
      * @return "The sum of all digits in 565 is 16"
      */
     public static String addDigits(int number) {
-
-        return "";
+        int a = number % 10;
+        int b = number / 10;
+        int c = b % 10;
+        int d = number /100;
+        int e = d % 10;
+        int sum = a + c + e;
+        String result3 = String.format("The sum of all digits in %d is %d", number, sum);
+        return result3;
     }
 
     /**
@@ -49,8 +59,11 @@ public class Assignment1 {
      * @return "3456789 minutes is approximately 6 years and 210 days"
      */
     public static String minsToYearsDays(int mins) {
-
-        return "";
+        int y = mins / 525600;
+        int a = mins % 525600;
+        int d = a / 1440;
+        String result4 = String.format("3456789 minutes is approximately %d years and %d days", y, d);
+        return result4;
     }
 
     /**
@@ -63,8 +76,10 @@ public class Assignment1 {
      * @return "Body Mass Index is 22.857"
      */
     public static String bmi(int kgs, double meters) {
-
-        return "";
+        double ms = meters * meters;
+        double b = kgs / ms;
+        String result5 = String.format("Body Mass Index is %.3f", b);
+        return result5;
     }
 
     /**
@@ -88,8 +103,20 @@ public class Assignment1 {
      *       Your speed in miles/h is 0.2615"
      */
     public static String speed(int meters, int hours, int minutes, int seconds) {
+        double mile = meters / 1609.0;
+        double km = meters / 1000.0;
+        double th = hours + (minutes / 60.0) + (seconds / 60.0 / 60.0);
+        double ts = seconds + (minutes * 60.0) + (hours * 60.0 * 60.0);
+        double ms = meters / ts;
+        double kh = km / th;
+        double mh = mile / th;
+        DecimalFormat df = new DecimalFormat();
+        df.setMinimumFractionDigits(4);
+        df.setRoundingMode(RoundingMode.DOWN);
 
-        return "";
+        String result6 = String.format("Your speed in meters/second is %.4f\nYour speed in km/h is %s\nYour " +
+                "speed in miles/h is %s", ms, df.format(kh), df.format(mh));
+        return result6;
     }
 
     /**
@@ -104,8 +131,11 @@ public class Assignment1 {
      *          Fourth power: 625"
      */
     public static String powers(int number) {
-
-        return "";
+        int sq = number * number;
+        int cu = sq * number;
+        int fp = cu * number;
+        String result7 = String.format("Square: %d\nCube: %d\nFourth power: %d", sq, cu, fp);
+        return result7;
     }
 
     /**
@@ -127,7 +157,23 @@ public class Assignment1 {
      *          Min integer: 5"
      */
     public static String arithmetic(int a, int b) {
-
-        return "";
+        int sum = a + b;
+        int dif = a - b;
+        int pro = a * b;
+        double ave = sum / 2.00;
+        int max;
+        int min;
+        int dis = Math.abs(a - b);
+        if (a > b){
+            max = a;
+            min = b;
+        }else {
+            max = b;
+            min = a;
+        }
+        String result8 = String.format("Sum of two integers: %d\nDifference of two integers: %d\nProduct of two " +
+                "integers: %d\nAverage of two integers: %.2f\nDistance of two integers: %d\nMax integer: %d\nMin " +
+                "integer: %d", sum, dif, pro, ave, dis, max, min);
+        return result8;
     }
 }
