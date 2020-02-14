38b273ebe01cf323463443c38dee25bf6cc489bd
version-1 <j.onodera26@gmail.com>
2020-02-13 18:45:10 -0800

    implement methods

--- src/main/java/ca/ciccc/Assignment1.java
+++ src/main/java/ca/ciccc/Assignment1.java
@@ -25,8 +25,9 @@ public class Assignment1 {
      * @return "1000.0 inches is 25.4 meters"
      */
     public static String inchesToMeters(int inches) {
-
-        return "";
+        double meters = inches / 39.3701;
+        String result = String.format("%.1f inches is %.1f meters", (double)inches, meters);
+        return result;
     }
 
     /**
@@ -37,8 +38,14 @@ public class Assignment1 {
      * @return "The sum of all digits in 565 is 16"
      */
     public static String addDigits(int number) {
-
-        return "";
+        String str = Integer.toString(number);
+        int sum = 0;
+        for (int i = 0; i <= str.length() - 1; i++) {
+            int num = Integer.parseInt(String.valueOf(str.charAt(i)));
+            sum += num;
+        }
+        String result = String.format("The sum of all digits in %s is %s", number, sum);
+        return result;
     }
 
     /**
@@ -49,8 +56,14 @@ public class Assignment1 {
      * @return "3456789 minutes is approximately 6 years and 210 days"
      */
     public static String minsToYearsDays(int mins) {
+        final int DAY = (60 * 24);
+        final int YEAR = 365 * DAY;
 
-        return "";
+        int year = mins / YEAR;
+        int days = (mins - (year * YEAR)) / DAY;
+        String result = String.format("%s minutes is approximately %s years and %s days", mins, year, days);
+
+        return result;
     }
 
     /**
@@ -63,8 +76,10 @@ public class Assignment1 {
      * @return "Body Mass Index is 22.857"
      */
     public static String bmi(int kgs, double meters) {
+        double bmi = kgs / (meters * meters);
+        String result = String.format("Body Mass Index is %.3f", bmi);
 
-        return "";
+        return result;
     }
 
     /**
@@ -88,8 +103,18 @@ public class Assignment1 {
      *       Your speed in miles/h is 0.2615"
      */
     public static String speed(int meters, int hours, int minutes, int seconds) {
+        double meters_per_second = meters / (double)(hours * 60 * 60 + minutes * 60 + seconds);
+        double km_per_hour = ((meters / (hours + (double)minutes / 60 + (double)seconds / 60 / 60)) * 10);
+        double meters_per_miles = km_per_hour / 1.609;
 
-        return "";
+        String result = String.format(
+            "Your speed in meters/second is %.4f\nYour speed in km/h is %.4f\nYour speed in miles/h is %.4f",
+            meters_per_second,
+            (int)km_per_hour / (double)10000,
+            (int)meters_per_miles / (double) 10000
+        );
+
+        return result;
     }
 
     /**
@@ -104,8 +129,18 @@ public class Assignment1 {
      *          Fourth power: 625"
      */
     public static String powers(int number) {
+        double square = Math.pow(number, 2);
+        double cube = Math.pow(number, 3);
+        double fourth_power = Math.pow(number, 4);
 
-        return "";
+        String result = String.format(
+            "Square: %s\nCube: %s\nFourth power: %s",
+            (int)square,
+            (int)cube,
+            (int)fourth_power
+        );
+
+        return result;
     }
 
     /**
@@ -127,7 +162,31 @@ public class Assignment1 {
      *          Min integer: 5"
      */
     public static String arithmetic(int a, int b) {
+        int sum = a + b;
+        int diff = a - b;
+        int times = a * b;
+        double average = (a + b) / 2;
+        int distance = a > b ? a - b : b - a;
+        int max = a > b ? a : b;
+        int min = a < b ? a : b;
 
-        return "";
+        String format = "Sum of two integers: %d\n" +
+                "Difference of two integers: %d\n" +
+                "Product of two integers: %d\n" +
+                "Average of two integers: %.2f\n" +
+                "Distance of two integers: %d\n" +
+                "Max integer: %d\nMin integer: %d";
+        String result = String.format(
+          format,
+          sum,
+          diff,
+          times,
+          average,
+          distance,
+          max,
+          min
+        );
+
+        return result;
     }
 }
