4814fb80c8a33bf4dae667c1abc93c52057b518b
HermiloLocal <hermilo.jh@gmail.com>
2020-02-14 04:23:32 -0800

    Finish Assignment 1

--- src/main/java/ca/ciccc/Assignment1.java
+++ src/main/java/ca/ciccc/Assignment1.java
@@ -1,83 +1,71 @@
 package ca.ciccc;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class Assignment1 {
-
     /**
      * Write a function to convert temperature from Fahrenheit to Celsius degree
      * Sample Input : 212
-     *
      * @param degree
      * @return "212.0 degree Fahrenheit is equal to 100.0 in Celsius"
      */
-    public static String fahrenheitToCelsius(double degree) {
-
+    public static String fahrenheitToCelsius(double degree) /* OK */ {
         double celsius = (degree - 32) * ((double) 5 / 9);
         String result = String.format("%.1f degree Fahrenheit is equal to %.1f in Celsius", degree, celsius);
-
         return result;
     }
-
     /**
      * Write a function program that takes an integer in inches, converts in to meters.
      * Sample Input : 1000
-     *
      * @param inches
      * @return "1000.0 inches is 25.4 meters"
      */
-    public static String inchesToMeters(int inches) {
-
-        return "";
+    public static String inchesToMeters(int inches) /* OK */ {
+        return String.format("%.1f inches is %.1f meters", ((double) inches), ((double) inches / 39.37));
     }
-
     /**
      * Write a function that takes an integer between 0 and 1000 and adds all the digits.
      * Sample Input : 565
-     *
      * @param number
      * @return "The sum of all digits in 565 is 16"
      */
-    public static String addDigits(int number) {
-
-        return "";
+    public static String addDigits(int number) /* OK */ {
+        int remainingNumber = 0;
+        int firstDigit = number % 10;           remainingNumber = number / 10;
+        int SecondDigit = remainingNumber % 10; remainingNumber = remainingNumber / 10;
+        int thirdDigit = remainingNumber % 10;  remainingNumber = remainingNumber / 10;
+        int fourthDigit = remainingNumber % 10;
+        return String.format("The sum of all digits in %d is %d", number, firstDigit + SecondDigit + thirdDigit + fourthDigit );
     }
-
     /**
      * Write a function that takes an integer as minutes and convert it to years and days.
      * Sample Input : 3456789
-     *
      * @param mins
      * @return "3456789 minutes is approximately 6 years and 210 days"
      */
-    public static String minsToYearsDays(int mins) {
-
-        return "";
+    public static String minsToYearsDays(int mins) /* OK */ {
+        return String.format("%d minutes is approximately %d years and %d days",mins,mins/525600,(mins - ((mins/525600)*525600)) / 1440);
     }
-
     /**
      * Write a function that takes weight as Kgs(Kilograms) and height as meters and compute
      * body mass index (BMI)
      * Sample Input: kgs = 70, meters = 1.75
-     *
      * @param kgs
      * @param meters
      * @return "Body Mass Index is 22.857"
      */
-    public static String bmi(int kgs, double meters) {
-
-        return "";
+    public static String bmi(int kgs, double meters) /* OK */ {
+        return String.format("Body Mass Index is %.3f", kgs/(meters*meters));
     }
-
     /**
      * Write a function that takes a distance (in meters) and the time was taken
      * (as three numbers: hours, minutes, seconds), and display the speed, in meters
-     * per second, kilometers per hour and miles per hour
-     * (hint: 1 mile = 1609 meters).
+     * per second, kilometers per hour and miles per hour (hint: 1 mile = 1609 meters).
      * Sample Input :
-     * meters = 2500
-     * hours = 5
-     * minutes = 56
-     * seconds = 23
-     *
+         * meters = 2500
+         * hours = 5
+         * minutes = 56
+         * seconds = 23
      * @param meters
      * @param hours
      * @param minutes
@@ -87,11 +75,16 @@ public class Assignment1 {
      *       Your speed in km/h is 0.4208
      *       Your speed in miles/h is 0.2615"
      */
-    public static String speed(int meters, int hours, int minutes, int seconds) {
-
-        return "";
+    public static String speed(int meters, int hours, int minutes, int seconds) /* OK */ {
+        double sumSeconds = (hours*3600.0f)+(minutes*60.0f)+seconds;
+        double mps = meters/sumSeconds;
+        double kph = (meters/1000.0f)/(sumSeconds/3600.0f);
+        double mph = kph/1.609f;
+        DecimalFormat df = new DecimalFormat("#.####");
+        df.setRoundingMode(RoundingMode.DOWN);
+        return String.format( "Your speed in meters/second is %.4f\nYour speed in km/h is %s\nYour speed in miles/h is %s",
+                mps,df.format(kph),df.format(mph));
     }
-
     /**
      * Write a function that reads a number and display the square,
      * cube, and the fourth power.
@@ -103,11 +96,12 @@ public class Assignment1 {
      *          Cube: 125
      *          Fourth power: 625"
      */
-    public static String powers(int number) {
-
-        return "";
+    public static String powers(int number) /* OK */ {
+        int square = number*number;
+        int cube   = square*number;
+        int fourth = cube*number;
+        return String.format("Square: %d\nCube: %d\nFourth power: %d", square, cube, fourth);
     }
-
     /**
      * Write a function that takes two integers a and b, then prints the sum,
      * the difference, the product, the average, the distance (absolute value of
@@ -126,8 +120,18 @@ public class Assignment1 {
      *          Max integer: 25
      *          Min integer: 5"
      */
-    public static String arithmetic(int a, int b) {
-
-        return "";
+    public static String arithmetic(int a, int b) /* OK */ {
+        int max, min;
+        if (a>b) { max = a; min = b; }
+        else { max = b; min = a; }
+        return String.format(
+                "Sum of two integers: %d\n" +
+                "Difference of two integers: %d\n" +
+                "Product of two integers: %d\n" +
+                "Average of two integers: %.2f\n" +
+                "Distance of two integers: %d\n" +
+                "Max integer: %d\n" +
+                "Min integer: %d",
+                a+b, a-b, a*b, (double)(a+b)/2, a-b, max, min);
     }
 }
--- src/test/java/Grader.java
+++ src/test/java/Grader.java
@@ -3,50 +3,46 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Grader {
-    @Test
+    @Test // OK
     public void testFahrenheitToCelsiusPositive() {
         String result = Assignment1.fahrenheitToCelsius(212);
         String expect = "212.0 degree Fahrenheit is equal to 100.0 in Celsius";
         Assert.assertEquals(expect, result);
     }
-
-    @Test
+    @Test // OK
     public void testInchesToMeters() {
         String result = Assignment1.inchesToMeters(1000);
         Assert.assertEquals("1000.0 inches is 25.4 meters", result);
     }
-
-    @Test
+    @Test // OK
     public void testAddDigits() {
         String result = Assignment1.addDigits(565);
         Assert.assertEquals("The sum of all digits in 565 is 16", result);
     }
-
-    @Test
+    @Test // OK
     public void testMinsToYearsDays() {
         String result = Assignment1.minsToYearsDays(3456789);
         Assert.assertEquals("3456789 minutes is approximately 6 years and 210 days", result);
     }
-
-    @Test
+    @Test // OK
     public void testBmi() {
         String result = Assignment1.bmi(70, 1.75);
         Assert.assertEquals("Body Mass Index is 22.857", result);
     }
-
-    @Test
+    @Test // OK
     public void testSpeed() {
         String result = Assignment1.speed(2500, 5, 56, 23);
-        Assert.assertEquals("Your speed in meters/second is 0.1169\nYour speed in km/h is 0.4208\nYour speed in miles/h is 0.2615", result);
+        Assert.assertEquals(
+                "Your speed in meters/second is 0.1169\n" +
+                "Your speed in km/h is 0.4208\n" +
+                "Your speed in miles/h is 0.2615", result);
     }
-
-    @Test
+    @Test // OK
     public void testPowers() {
         String result = Assignment1.powers(5);
         Assert.assertEquals("Square: 25\nCube: 125\nFourth power: 625", result);
     }
-
-    @Test
+    @Test // OK
     public void testArithmetic() {
         String result = Assignment1.arithmetic(25, 5);
         String expected = "Sum of two integers: 30\n" +
