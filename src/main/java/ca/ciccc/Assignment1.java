f4d2f020e72f5c97c41188641a9f7a4057338c40
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-10-08 15:14:04 -0700

    Complete assignment1

--- src/main/java/ca/ciccc/Assignment1.java
+++ src/main/java/ca/ciccc/Assignment1.java
@@ -1,5 +1,8 @@
 package ca.ciccc;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Assignment1 {
 
     /**
@@ -13,7 +16,6 @@ public class Assignment1 {
 
         double celsius = (degree - 32) * ((double) 5 / 9);
         String result = String.format("%.1f degree Fahrenheit is equal to %.1f in Celsius", degree, celsius);
-
         return result;
     }
 
@@ -25,9 +27,15 @@ public class Assignment1 {
      * @return "1000.0 inches is 25.4 meters"
      */
     public static String inchesToMeters(int inches) {
-
-        return "";
+        double metersPerInch = 0.0254;
+        double convertToMeters = (double)inches * metersPerInch;
+        return "1000.0 inches is " + convertToMeters + " meters";
     }
+
+    public static void main(String[] args) {
+        System.out.println(inchesToMeters(1000));
+    }
+
 
     /**
      * Write a function that takes an integer between 0 and 1000 and adds all the digits.
@@ -37,9 +45,16 @@ public class Assignment1 {
      * @return "The sum of all digits in 565 is 16"
      */
     public static String addDigits(int number) {
-
-        return "";
+        int sum = 0;
+        int num = number;
+        while(num > 0){
+            int remainder = num % 10;
+            sum = sum + remainder;
+            num = num / 10;
+        }
+        return "The sum of all digits in " + number + " is " + sum;
     }
+
 
     /**
      * Write a function that takes an integer as minutes and convert it to years and days.
@@ -49,10 +64,11 @@ public class Assignment1 {
      * @return "3456789 minutes is approximately 6 years and 210 days"
      */
     public static String minsToYearsDays(int mins) {
-
-        return "";
+        int dayNum = mins / (60 * 24);
+        int year = dayNum / 365;
+        int days = (dayNum % 365);
+        return mins + " minutes is approximately " + year + " years and " + days + " days";
     }
-
     /**
      * Write a function that takes weight as Kgs(Kilograms) and height as meters and compute
      * body mass index (BMI)
@@ -62,9 +78,12 @@ public class Assignment1 {
      * @param meters
      * @return "Body Mass Index is 22.857"
      */
+    // String.format() was referred to following URL.
+    // https://stackoverflow.com/questions/7278128/formatting-string-in-java-using-return-string-format
     public static String bmi(int kgs, double meters) {
-
-        return "";
+        double bmiVal = kgs / (meters * meters);
+        String answer = String.format("Body Mass Index is %.3f", bmiVal);
+        return answer;
     }
 
     /**
@@ -87,9 +106,23 @@ public class Assignment1 {
      *       Your speed in km/h is 0.4208
      *       Your speed in miles/h is 0.2615"
      */
-    public static String speed(int meters, int hours, int minutes, int seconds) {
 
-        return "";
+    // I referred to following URL regarding how to cut under decimal place 5.
+    // https://qiita.com/ota-meshi/items/967304d406d668febe1d
+    public static String speed(int meters, int hours, int minutes, int seconds) {
+        double metersSeconds = (double)meters / ((((hours * 60) + minutes) * 60) + seconds);
+        BigDecimal msSrc = BigDecimal.valueOf(metersSeconds);
+        BigDecimal msVal = msSrc.setScale(4, RoundingMode.DOWN);
+        double kilometersHours = (double)meters / 1_000 / (hours + ((double)minutes / 60) + ((double)seconds / (60 * 60)));
+        BigDecimal khSrc = BigDecimal.valueOf(kilometersHours);
+        BigDecimal khVal = khSrc.setScale(4, RoundingMode.DOWN);
+        double milesHours = (double)meters / 1_609 / (hours + ((double)minutes / 60) + ((double)seconds / (60 * 60)));
+        BigDecimal mhSrc = BigDecimal.valueOf(milesHours);
+        BigDecimal mhVal = mhSrc.setScale(4, RoundingMode.DOWN);
+
+        return "Your speed in meters/second is " + msVal +
+                "\nYour speed in km/h is " + khVal +
+                "\nYour speed in miles/h is " + mhVal;
     }
 
     /**
@@ -104,8 +137,10 @@ public class Assignment1 {
      *          Fourth power: 625"
      */
     public static String powers(int number) {
-
-        return "";
+        int square = number * number;
+        int cube = square * number;
+        int fourth = cube * number;
+        return "Square: " + square + "\nCube: "+ cube + "\nFourth power: " + fourth;
     }
 
     /**
@@ -127,7 +162,19 @@ public class Assignment1 {
      *          Min integer: 5"
      */
     public static String arithmetic(int a, int b) {
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double average = ((double)(a + b) / 2);
+        int distance = Math.abs(a - b);
+        int maxVal = Math.max(a, b);
+        int minVal = Math.min(a, b);
 
-        return "";
+        return "Sum of two integers: " + sum + "\n" +
+                "Difference of two integers: " + difference + "\n" +
+                "Product of two integers: " + product + "\n" +
+                "Average of two integers: " + String.format("%.2f", average) + "\n" +
+                "Distance of two integers: " + distance + "\n" +
+                "Max integer: " + maxVal + "\nMin integer: " + minVal;
     }
 }
