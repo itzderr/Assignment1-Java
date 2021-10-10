f78ce2831ad959d6c66975939ff718c8b94b6aa8
mau-io <maurice222@outlook.com>
2021-10-10 00:21:01 -0500

    Update Assignment 1 class

--- src/main/java/ca/ciccc/Assignment1.java
+++ src/main/java/ca/ciccc/Assignment1.java
@@ -10,7 +10,6 @@ public class Assignment1 {
      * @return "212.0 degree Fahrenheit is equal to 100.0 in Celsius"
      */
     public static String fahrenheitToCelsius(double degree) {
-
         double celsius = (degree - 32) * ((double) 5 / 9);
         String result = String.format("%.1f degree Fahrenheit is equal to %.1f in Celsius", degree, celsius);
 
@@ -25,8 +24,11 @@ public class Assignment1 {
      * @return "1000.0 inches is 25.4 meters"
      */
     public static String inchesToMeters(int inches) {
+        double meters = inches * 0.0254;
+        double i = Double.valueOf(inches);
+        String result = String.format("%.1f inches is %.1f meters", i, meters);
 
-        return "";
+        return result;
     }
 
     /**
@@ -37,8 +39,15 @@ public class Assignment1 {
      * @return "The sum of all digits in 565 is 16"
      */
     public static String addDigits(int number) {
+        int digits = number;
+        int sum = 0;
+        while (number != 0) {
+            sum = sum + (number % 10);
+            number = number / 10;
+        }
+        String result = "The sum of all digits in " + digits + " is " + sum;
 
-        return "";
+        return result;
     }
 
     /**
@@ -49,8 +58,12 @@ public class Assignment1 {
      * @return "3456789 minutes is approximately 6 years and 210 days"
      */
     public static String minsToYearsDays(int mins) {
+        int years = mins / 525600;
+        int days = (mins % 525600) / 1440;
+        String result  = mins + " minutes is approximately " + years
+                + " years and " + days + " days";
 
-        return "";
+        return result;
     }
 
     /**
@@ -63,8 +76,12 @@ public class Assignment1 {
      * @return "Body Mass Index is 22.857"
      */
     public static String bmi(int kgs, double meters) {
+        double weight = kgs;
+        double height = meters;
+        double bodyMassIndex = weight / Math.pow(height, 2);
+        String result = String.format("Body Mass Index is %.3f", bodyMassIndex);
 
-        return "";
+        return result;
     }
 
     /**
@@ -88,8 +105,13 @@ public class Assignment1 {
      *       Your speed in miles/h is 0.2615"
      */
     public static String speed(int meters, int hours, int minutes, int seconds) {
+        double timeSeconds = (hours * 3600) + (minutes * 60) + seconds;
+        double mps = meters / timeSeconds;
+        double kph = ( meters / 1000.0f ) / ( timeSeconds / 3600.0f );
+        double mph = kph / 1.609f;
+        String result = String.format("Your speed in meters/second is %.4f\nYour speed in km/h is %.6s\nYour speed in miles/h is %.6s", mps, kph, mph);
 
-        return "";
+        return result;
     }
 
     /**
@@ -104,8 +126,12 @@ public class Assignment1 {
      *          Fourth power: 625"
      */
     public static String powers(int number) {
+        int square = number * number;
+        int cube = number * number * number;
+        int power = (int)Math.pow(number, 4);
+        String result = "Square: "+ square + "\nCube: " + cube + "\nFourth power: " + power;
 
-        return "";
+        return result;
     }
 
     /**
@@ -127,7 +153,15 @@ public class Assignment1 {
      *          Min integer: 5"
      */
     public static String arithmetic(int a, int b) {
+        String result;
+        result = String.format("Sum of two integers: %d\n", a + b);
+        result += String.format("Difference of two integers: %d\n", a - b);
+        result += String.format("Product of two integers: %d\n", a * b);
+        result += String.format("Average of two integers: %.2f\n", (double) (a + b) / 2);
+        result += String.format("Distance of two integers: %d\n", Math.abs(a - b));
+        result += String.format("Max integer: %d\n", Math.max(a, b));
+        result += String.format("Min integer: %d", Math.min(a, b));
 
-        return "";
+        return result;
     }
 }
